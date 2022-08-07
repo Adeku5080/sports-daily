6484ea6680f99f962190eb2914331a3c1470afef
Adeku Ali <Aliadeku.aam@gmail.com>
2022-08-07 21:32:53 +0100

    add mobile responsiveness to the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,12 @@
 <head>
     <title>@yield('page-title', 'Welcome to My Blog')</title>
 
-{{--    <link rel="stylesheet" href="{{ secure_asset('vendor/css/style.css') }}/">--}}
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <link rel="stylesheet" href="{{asset("css/main.css")}}"/>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-              integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
-              crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+          integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+          crossorigin="anonymous"/>
 </head>
 
 <body>
@@ -21,19 +21,14 @@
 
         <div>
             <ul class="nav-links">
-{{--                @can('create', \App\Models\Post::class)--}}
-{{--                    <li>--}}
-{{--                        <a href="{{route('create')}}" class="addpost"> Add-post</a>--}}
-{{--                    </li>--}}
-{{--                @endcan--}}
 
                 @if(auth()->user()->is_admin)
 
-                        <li>
-                            <a href="{{route('create')}}" class="addpost"> Add-post</a>
-                        </li>
+                    <li>
+                        <a href="{{route('create')}}" class="addpost"> Add-post</a>
+                    </li>
 
-                    @endif
+                @endif
 
                 <li>
                     <form method="post" action="{{route('logout')}}">
@@ -44,13 +39,6 @@
             </ul>
         </div>
     </div>
-
-{{--    <div class="cell cell-2">--}}
-{{--     <h2>Most Recent Posts</h2>--}}
-{{--        @foreach($recentPosts as $recentPost)--}}
-{{--            <p class="recent-post"> {{ $recentPost->title }} </p>--}}
-{{--        @endforeach--}}
-{{--    </div>--}}
 
     <div class="content">
         @foreach($posts as $post)
@@ -67,25 +55,6 @@
         @endforeach
     </div>
 
-{{--    <div class="cell cell-4">--}}
-{{--        <h2>Most Popular Posts</h2>--}}
-{{--        @foreach($popularPosts as $popularPost)--}}
-{{--            <p class="recent-post">{{ $popularPost->title }} </p>--}}
-
-{{--        @endforeach--}}
-{{--    </div>--}}
-{{--    <div class="cell cell-5">--}}
-{{--        <div>--}}
-{{--            <a href="#"><i class="fab fa-facebook-f"></i></a>--}}
-{{--        </div>--}}
-{{--        <div>--}}
-{{--            <a href="https://twitter.com/Mo_jallo" target="_blank"><i class="fab fa-twitter"></i></a>--}}
-{{--        </div>--}}
-{{--        <div>--}}
-{{--            <a href="#"><i class="fab fa-instagram"></i></a>--}}
-
-{{--        </div>--}}
-{{--    </div>--}}
 </main>
 </body>
 
